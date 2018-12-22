987b161f16d0fefc494f3792fc9d6784eca9b7a6
foreverXZC <xiaozhenchenb@126.com>
2018-12-21 22:54:54 -0500

    Update Defense

--- MahjongAI/AIControllerDef.cs
+++ MahjongAI/AIControllerDef.cs
@@ -22,14 +22,14 @@ namespace MahjongAI
             return (gameData.players.Any(p => defLevel(p) >= 4)
                     && (evalResult.Distance >= 3
                         || evalResult.Distance == 2 && (evalResult.E_PromotionCount[0] <= 8 || evalResult.E_Point < 8000 || evalResult.E_PromotionCount[0] <= 15 && evalResult.E_Point < 12000)
-                        || evalResult.Distance == 1 && (evalResult.E_PromotionCount[0] <= 5 || evalResult.E_Point < 4000 || evalResult.E_PromotionCount[0] <= 9 && evalResult.E_Point < 8000)
-                        || (evalResult.Distance == 0 && evalResult.E_Point < 2000 || gameData.remainingTile / 4 <= evalResult.Distance * 2)
+                        || evalResult.Distance == 1 && (evalResult.E_PromotionCount[0] <= 4 || evalResult.E_Point < 4000 || evalResult.E_PromotionCount[0] <= 9 && evalResult.E_Point < 8000)
+                        || (evalResult.Distance == 0 && (evalResult.E_Point < 2000 || evalResult.E_PromotionCount[0] < 1) || gameData.remainingTile / 4 <= evalResult.Distance * 2)
                             && (discardTile == null || evalDef(discardTile).Risk > 15))
                 || gameData.players.Any(p => defLevel(p) >= 2)
                     && (evalResult.Distance >= 3
                         || evalResult.Distance == 2 && (evalResult.E_PromotionCount[0] <= 8 || evalResult.E_Point < 4000 || evalResult.E_PromotionCount[0] <= 15 && evalResult.E_Point < 12000)
-                        || evalResult.Distance == 1 && (evalResult.E_PromotionCount[0] <= 5 || evalResult.E_Point < 2000 || evalResult.E_PromotionCount[0] <= 9 && evalResult.E_Point < 8000)
-                        || (evalResult.Distance == 0 && evalResult.E_Point < 1000 || evalResult.Distance > 0 && gameData.remainingTile / 4 <= evalResult.Distance * 2)
+                        || evalResult.Distance == 1 && (evalResult.E_PromotionCount[0] <= 4 || evalResult.E_Point < 2000 || evalResult.E_PromotionCount[0] <= 9 && evalResult.E_Point < 8000)
+                        || (evalResult.Distance == 0 && (evalResult.E_Point < 1000 || evalResult.E_PromotionCount[0] < 1) || evalResult.Distance > 0 && gameData.remainingTile / 4 <= evalResult.Distance * 2)
                             && (discardTile == null || evalDef(discardTile).Risk > 15))
                 || gameData.players.Any(p => defLevel(p) >= 1)
                     && evalResult.Distance >= 2)
