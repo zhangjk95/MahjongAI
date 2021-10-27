dba0e028b31fc461a59ca3dac49b84c807c37542
Steven Vch <unStatiK@staremax.com>
2021-10-27 05:07:44 +0400

    fix formatting

--- MahjongAI/WebSocketNode.cs
+++ MahjongAI/WebSocketNode.cs
@@ -11,14 +11,14 @@ namespace MahjongAI
         private ClientWebSocket ws;
         private WebSocketUtils wsUtils = new WebSocketUtils();
         private string url;
+        
         public WebSocketNode(string url, Action<byte[], int> onMessage, Action<Exception> onError)
         {
             this.url = url;
-            
             Connect();
-            
             StartRecv(onMessage, onError);
         }
+        
         public async Task Send(byte[] buffer, bool gameEnded = false)
         {
             if (ws.State == WebSocketState.Open || ws.State == WebSocketState.CloseReceived)
@@ -40,14 +40,14 @@ namespace MahjongAI
         {
             wsUtils.Close(ws).Wait();
         }
+        
         private void ReconnectAndSend(byte[] buffer)
         {
             Connect();
-            
             Trace.WriteLine("Socket successfully reconnected, state=" + ws.State);
-            
             Send(buffer);
         }
+        
         private void Connect()
         {
             ws = wsUtils.Connect(url);
